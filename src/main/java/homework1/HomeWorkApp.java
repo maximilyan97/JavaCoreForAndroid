93a6eb5d8fd13039ebb2a87d2cd035b968d2ca5d
maluev <maluev@cmx.ru>
2022-07-04 14:58:52 +0300

    Домашнее задание №1 для курса "Java Core для Android".

---
+++ src/main/java/homework1/HomeWorkApp.java
@@ -0,0 +1,46 @@
+package homework1;
+
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    private static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+    }
+
+    private static void checkSumSign() {
+        int a = 3, b = 5;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    private static void printColor() {
+        int value = 5;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    private static void compareNumbers() {
+        int a = 2, b = 13;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+}
